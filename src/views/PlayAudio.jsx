f6544d565221ed5e7bfc9e71646f1dd26c5d7fba
Matthew Seo <rongjuni@hotmail.com>
2023-01-21 23:33:44 -0500

    audio double play fix

--- src/views/PlayAudio.jsx
+++ src/views/PlayAudio.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
+import { createGlobalStyle } from "styled-components";
 import "./PlayAudio.css";
 
 const PlayAudio = ({ selectedItem }) => {
+  
   let totalAudioNumber = 23;
   let randomGenerate = Math.floor(
     Math.random() * totalAudioNumber + 1
   ).toString();
   let randomThreeDigit = randomGenerate.padStart(3, "0");
   let [randomNumber, setRandomNumber] = useState(randomThreeDigit);
-
-  // const audio = new Audio("/voice/sound" + "200 copy" + ".m4a");
+  // let [randomNumber, setRandomNumber] = useState();
+  let [callOrEnd, setCallOrEnd] = useState("CALL");
 
   const audio = new Audio("/voice/sound" + randomNumber + ".m4a");
 
@@ -26,15 +28,26 @@ const PlayAudio = ({ selectedItem }) => {
         variant="danger"
         className="call-voice"
         onClick={() => {
-          audio.play();
-          let randomGenerate = Math.floor(
-            Math.random() * totalAudioNumber + 1
-          ).toString();
-          let randomThreeDigit = randomGenerate.padStart(3, "0");
-          setRandomNumber(randomThreeDigit);
+
+          if(audio.paused) {
+            audio.play();
+            // setCallOrEnd("End Call");
+          }
+          else if (!audio.paused) {
+            audio.pause();
+            // setCallOrEnd("CALL");
+            let randomGenerate = Math.floor(
+              Math.random() * totalAudioNumber + 1
+            ).toString();
+            let randomThreeDigit = randomGenerate.padStart(3, "0");
+            setRandomNumber(randomThreeDigit);
+            // setCallOrEnd("CALL");
+
+          } 
+            
         }}
       >
-        CALL
+        {callOrEnd}
       </Button>
     </div>
   );
