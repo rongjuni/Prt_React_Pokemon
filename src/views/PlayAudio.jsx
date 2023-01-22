32d979c3b2f595871905a4ce041fd905e829059a
Matthew Seo <rongjuni@hotmail.com>
2023-01-22 00:01:31 -0500

    audio play fix

--- src/views/PlayAudio.jsx
+++ src/views/PlayAudio.jsx
@@ -7,11 +7,10 @@ const PlayAudio = ({ selectedItem }) => {
   
   let totalAudioNumber = 23;
   let randomGenerate = Math.floor(
-    Math.random() * totalAudioNumber + 1
-  ).toString();
+    Math.random() * totalAudioNumber + 1)
+    .toString();
   let randomThreeDigit = randomGenerate.padStart(3, "0");
   let [randomNumber, setRandomNumber] = useState(randomThreeDigit);
-  // let [randomNumber, setRandomNumber] = useState();
   let [callOrEnd, setCallOrEnd] = useState("CALL");
 
   const audio = new Audio("/voice/sound" + randomNumber + ".m4a");
@@ -29,20 +28,17 @@ const PlayAudio = ({ selectedItem }) => {
         className="call-voice"
         onClick={() => {
 
-          if(audio.paused) {
+          if (audio.paused) {
             audio.play();
-            // setCallOrEnd("End Call");
           }
           else if (!audio.paused) {
             audio.pause();
             // setCallOrEnd("CALL");
-            let randomGenerate = Math.floor(
-              Math.random() * totalAudioNumber + 1
-            ).toString();
-            let randomThreeDigit = randomGenerate.padStart(3, "0");
-            setRandomNumber(randomThreeDigit);
-            // setCallOrEnd("CALL");
-
+            // let randomGenerate = Math.floor(
+            //   Math.random() * totalAudioNumber + 1
+            // ).toString();
+            // let randomThreeDigit = randomGenerate.padStart(3, "0");
+            // setRandomNumber(randomThreeDigit);
           } 
             
         }}
